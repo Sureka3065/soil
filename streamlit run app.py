1b61d6378d7890fb894a605c9670f6cb663aa4b4
Sureka3065 <123437228+Sureka3065@users.noreply.github.com>
2025-11-19 19:29:31 +0530

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import ExtraTreesRegressor
 from catboost import CatBoostRegressor
